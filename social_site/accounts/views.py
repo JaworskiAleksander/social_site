93ecd1e28ae21f83d5b739247feb8e1139ff9a2d
Aleksander Jaworski <aleck156@gmail.com>
2020-03-02 09:56:56 +0100

    created SignUp view stub;

--- social_site/accounts/views.py
+++ social_site/accounts/views.py
@@ -1,3 +1,11 @@
+# Django imports
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+# app imports
+from . import forms
 
 # Create your views here.
+class SignUp(CreateView):
+    pass
