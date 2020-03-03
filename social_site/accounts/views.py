8e9ad9e13bbda44b797d5a25e358e1f15ae095cd
Aleksander Jaworski <aleck156@gmail.com>
2020-03-03 10:13:06 +0100

    fixed missing / in accounts.views.py

--- social_site/accounts/views.py
+++ social_site/accounts/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from django.contrib.auth import login, logout
 # app imports
 from . import forms
 
--- social_site/social_site/views.py
+++ social_site/social_site/views.py
@@ -11,4 +11,9 @@ class ThanksPage(TemplateView):
 
 class HomePage(TemplateView):
     template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('test'))
+        return super().get(request, *args, **kwargs)
     
