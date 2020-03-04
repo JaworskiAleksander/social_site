01efeaea9914c4bfa4ec64a36894ebbdbdec1ecb
Aleksander Jaworski <aleck156@gmail.com>
2020-03-04 09:32:23 +0100

    added import of Group and GroupMember in groups.views.py

--- social_site/groups/views.py
+++ social_site/groups/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.mixins import (
 from django.urls import reverse
 from django.db import IntegrityError
 from django.views import generic
+from groups.models import Group, GroupMember
 from . import models
 
 
@@ -15,6 +16,6 @@ from . import models
 # when someone logged in the system wants to create a group ...
 # login required + createview
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
-    fields = ('name','description') # those fields will be shown with a form tag
+    fields = ('name','description') # only those fields will be shown with a form tag
     model = Group # this model will be used to store data in db.sqlite3
 
