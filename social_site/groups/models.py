a67e6db8fd33ec18c1a21b69466c8d5a7cfa6ea4
Aleksander Jaworski <aleck156@gmail.com>
2020-03-04 08:41:24 +0100

    completed Group class in models.py

--- social_site/groups/models.py
+++ social_site/groups/models.py
@@ -12,7 +12,28 @@ from django import template
 register = template.Library()
 
 class Group(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+    slug = models.SlugField(allow_unicode=True, unique = True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User, through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs={"slug": self.slug})
+
+    class Meta:
+        ordering = ["name"]
+
     pass
+
 
 class GroupMember(models.Model):
     group = models.ForeignKey(Group, related_name='membership', on_delete=models.CASCADE)
@@ -23,6 +44,3 @@ class GroupMember(models.Model):
 
     class Meta:
         unique_together = ('group', 'user')
-
-    
-    pass
