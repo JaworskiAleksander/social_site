7955cbe78401d4b28248b7ca2769df25b1b0de78
Aleksander Jaworski <aleck156@gmail.com>
2020-03-04 09:04:10 +0100

    created class Post in posts.models.py

--- social_site/posts/models.py
+++ social_site/posts/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
 
 # Create your models here.
+import misaka
+
+from group.models import Group
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class Post(models.Model):
+    # User is known thanks to get_user_model; django takes care of that for us!
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, related_name='posts', null=True, blank=True, on_delete=models.CASCADE)
+
+# get absolute url - when someone posts something, this is where they'll be sent through
+    # methods
+    def __str__(self):
+        return self.message
+
+    # when someone writes using markdown or posts a link, it gets properly supported
+    def save(*args, **kwargs):
+        self.message_html = misaka(self.message)
+        super().save(**args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse(
+            'posts:single',
+            kwargs={
+                'username': self.user.username,
+                'pk': self.pk
+            }
+        )
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['user', 'message'] # this can also be a tuple
+    
+    
