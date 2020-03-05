128fc3eca149fc0f08adb61d88ec1d7da57e9c7a
Aleksander Jaworski <aleck156@gmail.com>
2020-03-05 08:46:20 +0100

    registered Group and GroupMember models in admin.py

--- social_site/groups/admin.py
+++ social_site/groups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+# use admin interface to edit models on the same page as the parent model
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
