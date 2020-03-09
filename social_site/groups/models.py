60fee1ae8e3ef099c961c617c23070ed12942cf6
Aleksander Jaworski <aleck156@gmail.com>
2020-03-09 09:50:03 +0100

    added imports in groups.models.py

--- social_site/groups/models.py
+++ social_site/groups/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.text import slugify   # allows to remove unwanted characters
+from django.urls import reverse
+from django.conf import settings
 
 
 # Create your models here.
