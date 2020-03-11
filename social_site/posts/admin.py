12e8a1163cdc0f6640bbfa11a029b8445c39ad9f
Aleksander Jaworski <aleck156@gmail.com>
2020-03-11 16:23:54 +0100

    added a simple filter in Posts view in admin site

--- social_site/posts/admin.py
+++ social_site/posts/admin.py
@@ -4,5 +4,6 @@ from . import models
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     search_fields = ['user']
+    list_filter = ['group', 'user']
 
 admin.site.register(models.Post, PostAdmin)
