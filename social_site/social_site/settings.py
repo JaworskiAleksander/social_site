3bd759e51667ca32914fc2240ea978af9f28e31c
Aleksander Jaworski <aleck156@gmail.com>
2020-03-11 15:06:25 +0100

    added debug-toolbar

--- social_site/social_site/settings.py
+++ social_site/social_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'django.contrib.humanize',
     'bootstrap4',
     'accounts.apps.AccountsConfig',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'social_site.urls'
